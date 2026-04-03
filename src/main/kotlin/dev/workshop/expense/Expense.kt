ecff05b52a8d5902c167298785b4ea617f6b7440
hermanngeorge15 <hermann.george15@gmail.com>
2026-04-03 13:35:27 +0200

    Step: step-06-start

--- src/main/kotlin/dev/workshop/expense/Expense.kt
+++
@@ -1,8 +0,0 @@
-package dev.workshop.expense
-
-data class Expense(
-    val id: Int,
-    val description: String,
-    val amount: Int,
-    val date: String
-)
--- src/main/kotlin/dev/workshop/expense/ExpenseNotFoundException.kt
+++
@@ -1,4 +0,0 @@
-package dev.workshop.expense
-
-// TODO: Vytvorte tridu ExpenseNotFoundException ktera dedi z RuntimeException
-// Hint: class ExpenseNotFoundException(id: Int) : RuntimeException("Vydaj s ID $id nebyl nalezen")
